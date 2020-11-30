cfef54e3c66addab0e4b663046fc5541ce836837
Neil Foster <neilfoster81@gmail.com>
2020-11-30 15:21:17 +0000

    made hamburger turn into X when toggled

--- script.js
+++ script.js
@@ -42,8 +42,10 @@ burger.addEventListener('click', () =>{
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
         }
     });
+    //Burger animation
+    burger.classList.toggle('toggle')
 });
-    //Animate Links
+    
     
 
 }
